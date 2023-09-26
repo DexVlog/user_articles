74f0b4d55d967f9ca250afaf7d2f5e4118764f81
Kacper Majcher <kacpermajcher13@gmail.com>
2023-09-26 21:13:29 +0200

    Implement freezed in articles and home state's

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,8 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:user_articles/app/core/enums.dart';
 import 'package:user_articles/domain/models/author_model.dart';
 import 'package:user_articles/domain/repositories/authors_repository.dart';
 
+part 'home_cubit.freezed.dart';
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
