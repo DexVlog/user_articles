226e6137d0985ac59e777b1ee294cdc2f55e9c72
Kacper Majcher <kacpermajcher13@gmail.com>
2023-10-01 13:42:55 +0200

    Add tests to home_cubit

--- lib/app/injection_container.config.dart
+++ lib/app/injection_container.config.dart
@@ -4,18 +4,28 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+// ignore: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
+// ignore: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
+// ignore: no_leading_underscores_for_library_prefixes
 import 'package:injectable/injectable.dart' as _i2;
 
+// ignore: no_leading_underscores_for_library_prefixes
 import '../data/remote_data_sources/articles_remote_data_source.dart' as _i4;
+// ignore: no_leading_underscores_for_library_prefixes
 import '../data/remote_data_sources/authors_remote_data_source.dart' as _i6;
+// ignore: no_leading_underscores_for_library_prefixes
 import '../domain/repositories/articles_repository.dart' as _i5;
+// ignore: no_leading_underscores_for_library_prefixes
 import '../domain/repositories/authors_repository.dart' as _i7;
+// ignore: no_leading_underscores_for_library_prefixes
 import '../features/articles/cubit/articles_cubit.dart' as _i9;
+// ignore: no_leading_underscores_for_library_prefixes
 import '../features/home/cubit/home_cubit.dart' as _i8;
 import 'injection_container.dart'
-    as _i10; // ignore_for_file: unnecessary_lambdas
+    // ignore: no_leading_underscores_for_library_prefixes
+    as _i10;
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
